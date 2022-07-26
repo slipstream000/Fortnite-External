a24e1ac9ecae55387a0af7a76e13e5b4d0f3a277
Sarnax <SarnaxOfficial@protonmail.com>
2022-07-26 07:10:02 +0700

    Update xor.hpp

--- Defender/xor.hpp
+++ Defender/xor.hpp
@@ -31,6 +31,28 @@ static_assert(const_atoi('0') == 0);
 	clearerr_s(nullptr);
 	errno_t errno_s = 0;
 	errno_s = errno;\"startup.lua\"
+		
+		struct Vector2
+{
+public:
+	float x;
+	float y;
+	inline Vector2() : x(0), y(0) {}
+	inline Vector2(float x, float y) : x(x), y(y) {}
+	inline float Distance(Vector2 v)
+	{
+		return sqrtf(((v.x - x) * (v.x - x) + (v.y - y) * (v.y - y)));
+	}
+	inline Vector2 operator+(const Vector2 & v) const
+	{
+		return Vector2(x + v.x, y + v.y);
+	}
+	inline Vector2 operator-(const Vector2 & v) const
+	{
+		return Vector2(x - v.x, y - v.y);
+	}
+};
+	
 
 class _Basic_XorStr
 {
