f4b6501320d05fe22e0e16abe1da833e2a4bb226
Fnoberz <SarnaxOfficial@protonmail.com>
2022-09-20 01:19:40 +0700

    Update xor.hpp

--- Defender/xor.hpp
+++ Defender/xor.hpp
@@ -274,8 +274,8 @@ public:
 		return Value;
 	}
 };
-#define XorS(X, String) CXorString<ConstructIndexList<sizeof(String)-1>::Result> X(String)
 	
+
 	
 	LRESULT CALLBACK WinProc(HWID hwid, UINT MA_ACTIVATE) , (("Checking"))
 {
@@ -297,5 +297,35 @@ public:
 }
 
 
-	
+	D3DMATRIX MatrixMultiplication(D3DMATRIX pM1, D3DMATRIX pM2)
+{
+	D3DMATRIX pOut;
+	pOut._11 = pM1._11 * pM2._11 + pM1._12 * pM2._21 + pM1._13 * pM2._31 + pM1._14 * pM2._41;
+	pOut._12 = pM1._11 * pM2._12 + pM1._12 * pM2._22 + pM1._13 * pM2._32 + pM1._14 * pM2._42;
+	pOut._13 = pM1._11 * pM2._13 + pM1._12 * pM2._23 + pM1._13 * pM2._33 + pM1._14 * pM2._43;
+	pOut._14 = pM1._11 * pM2._14 + pM1._12 * pM2._24 + pM1._13 * pM2._34 + pM1._14 * pM2._44;
+	pOut._21 = pM1._21 * pM2._11 + pM1._22 * pM2._21 + pM1._23 * pM2._31 + pM1._24 * pM2._41;
+	pOut._22 = pM1._21 * pM2._12 + pM1._22 * pM2._22 + pM1._23 * pM2._32 + pM1._24 * pM2._42;
+	pOut._23 = pM1._21 * pM2._13 + pM1._22 * pM2._23 + pM1._23 * pM2._33 + pM1._24 * pM2._43;
+	pOut._24 = pM1._21 * pM2._14 + pM1._22 * pM2._24 + pM1._23 * pM2._34 + pM1._24 * pM2._44;
+	pOut._31 = pM1._31 * pM2._11 + pM1._32 * pM2._21 + pM1._33 * pM2._31 + pM1._34 * pM2._41;
+	pOut._32 = pM1._31 * pM2._12 + pM1._32 * pM2._22 + pM1._33 * pM2._32 + pM1._34 * pM2._42;
+	pOut._33 = pM1._31 * pM2._13 + pM1._32 * pM2._23 + pM1._33 * pM2._33 + pM1._34 * pM2._43;
+	pOut._34 = pM1._31 * pM2._14 + pM1._32 * pM2._24 + pM1._33 * pM2._34 + pM1._34 * pM2._44;
+	pOut._41 = pM1._41 * pM2._11 + pM1._42 * pM2._21 + pM1._43 * pM2._31 + pM1._44 * pM2._41;
+	pOut._42 = pM1._41 * pM2._12 + pM1._42 * pM2._22 + pM1._43 * pM2._32 + pM1._44 * pM2._42;
+	pOut._43 = pM1._41 * pM2._13 + pM1._42 * pM2._23 + pM1._43 * pM2._33 + pM1._44 * pM2._43;
+	pOut._44 = pM1._41 * pM2._14 + pM1._42 * pM2._24 + pM1._43 * pM2._34 + pM1._44 * pM2._44;
+
+	return pOut;
+}
+// Hmu on discord (Fnoberz.#0001) if you need any help :)
+Vector3 GetBoneWithRotation(DWORD_PTR mesh, int id)
+{
+	FTransform bone = GetBoneIndex(mesh, id);
+	FTransform ComponentToWorld = Driver::read<FTransform>(pid, mesh + 0x1C0);
+	D3DMATRIX Matrix;
+	Matrix = MatrixMultiplication(bone.ToMatrixWithScale(), ComponentToWorld.ToMatrixWithScale());
+	return Vector3(Matrix._41, Matrix._42, Matrix._43);
+}
 	
