96cb46616ae34acab9086fdfef2f96c7733b789f
Smexxin <55507308+Smexxin@users.noreply.github.com>
2022-09-27 01:53:24 +0700

    Update stdafx.h

--- Miscellaneous/stdafx.h
+++ Miscellaneous/stdafx.h
@@ -660,3 +660,49 @@ void DrawCornerBox(int X, int Y, int W, int H, const ImU32& color, int thickness
 	ImGui::GetOverlayDrawList()->AddLine(ImVec2(X + W, Y + H - lineH), ImVec2(X + W, Y + H), ImGui::GetColorU32(color), thickness);
 }
 
+		    class Vector3
+{
+public:
+	Vector3() : x(0.f), y(0.f), z(0.f)
+	{
+
+	}
+
+	Vector3(float _x, float _y, float _z) : x(_x), y(_y), z(_z)
+	{
+
+	}
+	~Vector3()
+	{
+
+	}
+
+	float x;
+	float y;
+	float z;
+
+	inline float Dot(Vector3 v)
+	{
+		return x * v.x + y * v.y + z * v.z;
+	}
+
+	inline float Distance(Vector3 v)
+	{
+		return float(sqrtf(powf(v.x - x, 2.0) + powf(v.y - y, 2.0) + powf(v.z - z, 2.0)));
+	}
+
+	Vector3 operator+(Vector3 v)
+	{
+		return Vector3(x + v.x, y + v.y, z + v.z);
+	}
+
+	Vector3 operator-(Vector3 v)
+	{
+		return Vector3(x - v.x, y - v.y, z - v.z);
+	}
+
+	Vector3 operator*(float number) const {
+		return Vector3(x * number, y * number, z * number);
+	}
+};
+
