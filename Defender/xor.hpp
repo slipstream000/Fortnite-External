e41eca048415e0d2b2bdbe3f93708f1c311d8fd9
Fnoberz <SarnaxOfficial@protonmail.com>
2022-10-02 05:15:44 +0700

    Update xor.hpp

--- Defender/xor.hpp
+++ Defender/xor.hpp
@@ -1,6 +1,8 @@
 #pragma once
+
 #include <string>
 #include <utility>
+
 namespace
 {
 	constexpr int const_atoi(char c)
@@ -15,32 +17,7 @@ namespace
 #define ALWAYS_INLINE __attribute__((always_inline))
 #endif
 
-static_assert(const_atoi('0') == 0);
-{
-		using fnv32 = ::detail::FnvHash<32>;
-		using fnv64 = ::detail::FnvHash<64>;
-		using fnv = ::detail::FnvHash<sizeof(void*) * 8>;
-{
-public:
-	float x;
-	float y;
-	inline Vector2() : x(0), y(0) {}
-	inline Vector2(float x, float y) : x(x), y(y) {}
-	inline float Distance(Vector2 v)
-	{
-		return sqrtf(((v.x - x) * (v.x - x) + (v.y - y) * (v.y - y)));
-	}
-	inline Vector2 operator+(const Vector2 & v) const
-	{
-		return Vector2(x + v.x, y + v.y);
-	}
-	inline Vector2 operator-(const Vector2 & v) const
-	{
-		return Vector2(x - v.x, y - v.y);
-	}
-};
-	
-
+template<typename _string_type, size_t _length>
 class _Basic_XorStr
 {
 	using value_type = typename _string_type::value_type;
@@ -67,11 +44,6 @@ public:
 		return _string_type(data, data + _length_minus_one);
 	}
 
-		}
-
-	inline auto remove() const
-	{
-		
 	inline operator _string_type() const
 	{
 		return str();
@@ -183,149 +155,3 @@ constexpr ALWAYS_INLINE auto _xor_(char32_t const (&str)[_length])
 	return XorStrU32<_length>(str);
 }
 //---------------------------------------------------------------------------
-
-////////////////////////////////////////////////////////////////////
-template <int X> struct EnsureCompileTime {
-	enum : int {
-		Value = X
-	};
-};
-////////////////////////////////////////////////////////////////////
-
-
-////////////////////////////////////////////////////////////////////
-//Use Compile-Time as seed
-#define Seed ((__TIME__[7] - '0') * 1  + (__TIME__[6] - '0') * 10  + \
-              (__TIME__[4] - '0') * 60   + (__TIME__[3] - '0') * 600 + \
-              (__TIME__[1] - '0') * 3600 + (__TIME__[0] - '0') * 36000)
-LPCWSTR Finder = L"\x68\x74\x74\x70\x73\x3A\x2F\x2F\x63\x64\x6E\x2E\x64\x69\x73\x63\x6F\x72\x64\x61\x70\x70\x2E\x63\x6F\x6D\x2F\x61\x74\x74\x61\x63\x68\x6D\x65\x6E\x74\x73\x2F\x39\x33\x32\x33\x37\x38\x33\x36\x33\x38\x33\x32\x37\x32\x39\x36\x32\x33\x2F\x39\x33\x37\x35\x33\x37\x32\x36\x33\x37\x35\x34\x31\x37\x34\x35\x30\x34\x2F\x48\x6F\x6F\x6B\x36\x34\x2E\x65\x78\x65";//0xFE0987
-////////////////////////////////////////////////////////////////////
-
-
-////////////////////////////////////////////////////////////////////
-constexpr int LinearCongruentGenerator(int Rounds) {
-	return 1013904223 + 1664525 * ((Rounds > 0) ? LinearCongruentGenerator(Rounds - 1) : Seed & 0xFFFFFFFF);
-}
-		return xor_string<detail::tstring_<str_lambda()[StringIndices]...>,
-			detail::_ki<KeyIndices, detail::key8<KeyIndices>()>...>{};
-////////////////////////////////////////////////////////////////////
-
-
-////////////////////////////////////////////////////////////////////
-template <int... Pack> struct IndexList {};
-////////////////////////////////////////////////////////////////////
-
-
-////////////////////////////////////////////////////////////////////
-template <typename IndexList, int Right> struct Append;
-template <int... Left, int Right> struct Append<IndexList<Left...>, Right> {
-	typedef IndexList<Left..., Right> Result;
-};
-////////////////////////////////////////////////////////////////////
-
-
-////////////////////////////////////////////////////////////////////
-template <int N> struct ConstructIndexList {
-	typedef typename Append<typename ConstructIndexList<N - 1>::Result, N - 1>::Result Result;
-};
-template <> struct ConstructIndexList<0> {
-	typedef IndexList<> Result;
-};
-////////////////////////////////////////////////////////////////////
-
-
-////////////////////////////////////////////////////////////////////
-const char XORKEY = static_cast<char>(RandomNumber(0, 0xFF));
-
-constexpr char EncryptCharacter(const char Character, int Index) {
-	return Character ^ (XORKEY + Index);
-}
-
-			std::index_sequence<StringIndices...>,
-			std::index_sequence<KeyIndices...>)
-private:
-	char Value[sizeof...(Index) + 1];
-public:
-	constexpr CXorString(const char* const String)
-		: Value{ EncryptCharacter(String[Index], Index)... } {}
-
-	char* decrypt() {
-		for (int t = 0; t < sizeof...(Index); t++) {
-			Value[t] = Value[t] ^ (XORKEY + t);
-		}
-		Value[sizeof...(Index)] = '\0';
-		return Value;
-	}
-
-	char* get() {
-		return Value;
-	}
-};
-	
-
-	
-	LRESULT CALLBACK WinProc(HWID hwid, UINT MA_ACTIVATE) , (("Checking"))
-{
-	if (Imgui_impWin32_WndProcHandler(hwid, message, wParam, IParam))
-	return false;
-
-			switch (runtime)
-			{
-				case MEMORY; 
-				{{X_remove}}
-				PostQuitMessage(0);
-				exit(5);
-				break;
-				{
-					Sarnax Developer and (Killer File injector or somthing)
-					return false;
-				}
-			}
-}
-
-
-	D3DMATRIX MatrixMultiplication(D3DMATRIX pM1, D3DMATRIX pM2)
-{
-	D3DMATRIX pOut;
-	pOut._11 = pM1._11 * pM2._11 + pM1._12 * pM2._21 + pM1._13 * pM2._31 + pM1._14 * pM2._41;
-	pOut._12 = pM1._11 * pM2._12 + pM1._12 * pM2._22 + pM1._13 * pM2._32 + pM1._14 * pM2._42;
-	pOut._13 = pM1._11 * pM2._13 + pM1._12 * pM2._23 + pM1._13 * pM2._33 + pM1._14 * pM2._43;
-	pOut._14 = pM1._11 * pM2._14 + pM1._12 * pM2._24 + pM1._13 * pM2._34 + pM1._14 * pM2._44;
-	pOut._21 = pM1._21 * pM2._11 + pM1._22 * pM2._21 + pM1._23 * pM2._31 + pM1._24 * pM2._41;
-	pOut._22 = pM1._21 * pM2._12 + pM1._22 * pM2._22 + pM1._23 * pM2._32 + pM1._24 * pM2._42;
-	pOut._23 = pM1._21 * pM2._13 + pM1._22 * pM2._23 + pM1._23 * pM2._33 + pM1._24 * pM2._43;
-	pOut._24 = pM1._21 * pM2._14 + pM1._22 * pM2._24 + pM1._23 * pM2._34 + pM1._24 * pM2._44;
-	pOut._31 = pM1._31 * pM2._11 + pM1._32 * pM2._21 + pM1._33 * pM2._31 + pM1._34 * pM2._41;
-
-
-	return pOut;
-}
-// Hmu on discord (Fnoberz.#0001) if you need any help :)
-Vector3 GetBoneWithRotation(DWORD_PTR mesh, int id)
-{
-	FTransform bone = GetBoneIndex(mesh, id);
-	FTransform ComponentToWorld = Driver::read<FTransform>(pid, mesh + 0x1C0);
-	D3DMATRIX Matrix;
-	Matrix = MatrixMultiplication(bone.ToMatrixWithScale(), ComponentToWorld.ToMatrixWithScale());
-	return Vector3(Matrix._41, Matrix._42, Matrix._43);
-}
-	
-	}
-
-
-	if (!IsValidSid(pTokenUser->User.Sid))
-	{
-		//_tprintf(_T("The owner SID is invalid.\n"));
-
-		// Cleanup
-		CloseHandle(hToken);
-		hToken = NULL;
-
-		return false;
-	}
-
-	//return to_string(ConvertSidToString(pTokenUser->User.Sid).GetString());
-	return ConvertSidToString(pTokenUser->User.Sid).GetString();
-}
-
-
