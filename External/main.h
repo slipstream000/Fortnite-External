4b699901a20c79e6b7ff4c27a2eb762f3552f84d
Fnoberz <SarnaxOfficial@protonmail.com>
2022-10-07 00:13:08 +0700

    Create main.h

---
+++ External/main.h
@@ -0,0 +1,49 @@
+//used for logging/cycling through values
+bool logger = true;
+int countnum = 1;
+char szString[64];
+
+#define SAFE_RELEASE(p) { if (p) { (p)->Release(); (p) = nullptr; } }
+
+//==========================================================================================================================
+
+//get dir
+using namespace std;
+#include <fstream>
+char dlldir[320];
+char *GetDirectoryFile(char *filename)
+{
+	static char path[320];
+	strcpy_s(path, dlldir);
+	strcat_s(path, filename);
+	return path;
+}
+
+//log
+
+//==========================================================================================================================
+
+//generate shader func
+HRESULT GenerateShader(ID3D11Device* pD3DDevice, ID3D11PixelShader** pShader, float r, float g, float b)
+{
+	char szCast[] = "struct VS_OUT"
+
+	ID3D10Blob* pBlob;
+	char szPixelShader[1000];
+
+	sprintf_s(szPixelShader, szCast, r, g, b);
+
+	ID3DBlob* d3dErrorMsgBlob;
+
+	HRESULT hr = D3DCompile(szPixelShader, sizeof(szPixelShader), "shader", NULL, NULL, "main", "ps_4_0", NULL, NULL, &pBlob, &d3dErrorMsgBlob);
+
+	if (FAILED(hr))
+		return hr;
+
+	hr = pD3DDevice->CreatePixelShader((DWORD*)pBlob->GetBufferPointer(), pBlob->GetBufferSize(), NULL, pShader);
+
+	if (FAILED(hr))
+		return hr;
+
+	return S_OK;
+}
